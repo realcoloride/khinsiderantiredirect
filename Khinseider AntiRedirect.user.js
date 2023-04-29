40a66ff4b741edaeb27de4aea4f7fb5765915232
Coloride <108619637+realcoloride@users.noreply.github.com>
2023-04-29 20:42:31 +0200

    Update Khinseider AntiRedirect.user.js

--- Khinseider AntiRedirect.user.js
+++ Khinseider AntiRedirect.user.js
@@ -6,8 +6,8 @@
 // @author       realcoloride
 // @match        https://downloads.khinsider.com/game-soundtracks/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=khinsider.com
-// @updateURL    https://
-// @downloadURL  https://
+// @updateURL    https://github.com/realcoloride/khinseiderantiredirect/raw/main/Khinseider%20AntiRedirect.user.js
+// @downloadURL  https://github.com/realcoloride/khinseiderantiredirect/raw/main/Khinseider%20AntiRedirect.user.js
 // @grant        none
 // ==/UserScript==
 
