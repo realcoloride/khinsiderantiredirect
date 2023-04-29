be558b18b68e1487145441e90e36705311d7e204
Coloride <108619637+realcoloride@users.noreply.github.com>
2023-04-29 20:54:43 +0200

    Fix typo

--- Khinsider AntiRedirect.user.js
+++ Khinsider AntiRedirect.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Khinseider AntiRedirect
+// @name         Khinsider AntiRedirect
 // @namespace    https://downloads.khinsider.com/game-soundtracks/*
 // @version      0.2
 // @description  Makes khinseider stop redirecting you when clicking download on a soundtrack. Also directly downloads the soundtracks instead of redirecting and includes a right click save as directly from the album's page.
